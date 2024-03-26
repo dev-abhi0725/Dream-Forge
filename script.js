74d6f1c1f9dc6cac9b6066d87464bbc9d3ec66fd
dev-abhi0725 <abhishekmandal0725@gmail.com>
2024-03-26 13:17:33 +0530

    Added files and updated existing files

--- script.js
+++ script.js
@@ -1,31 +1,85 @@
+var slideImg = document.getElementById("slideImg");
+
+var images = new Array(
+    "assets/images/img1.jpg",
+    "assets/images/img2.avif",
+    "assets/images/img3.jpg",
+    "assets/images/img4.jpg"
+);
+
+var len = images.length;
+var i=0;
+function slider(){
+    if(i>len-1){
+        i=0;
+    }
+    slideImg.src = images[i];
+    i++;
+    setTimeout('slider()',3000)
+}
+
+
+
+
+
+
+
+
+
+
+
 var tl= gsap.timeline();
-tl.from(".navbar1 li,.navbar2 li",{
+tl.from(".header",{
     y:-100,
     delay:.3,
     stagger:.2
 })
 
-tl.from(".content h1, .content p",{
+tl.from(".content h1,.content .note1",{
     y:100,
     opacity:0,
     stagger:.3
 })
-tl.from(".image img",{
-    scale:0,
-    opacity:0,
-    duration:.3,
-    stagger:.3
-})
-tl.from(".lower button",{
+tl.from(".content button",{
     scale :0,
     opacity: 0
 })
-tl.to(".lower button",{
+tl.to(".content button",{
     y:20,
     repeat:-1,
     yoyo:true
 })
-tl.from(".lower p,.lower hr",{
+tl.from(".note2,.line, .note3",{
     y:100,
-    opacity:0,stagger:.3
+    opacity:0,
+    stagger:.3
 })
+tl.from(".content h5",{
+    scale:0,
+    opacity:0
+})
+tl.to("h5",{
+    y:30,
+    repeat:-1,
+    yoyo:true
+})
+tl.from(".page2 h2 ",{
+    y:100,
+    opacity:0,
+    stagger:.3
+})
+tl.from(".card ",{
+    y:100,
+    opacity:0,
+    stagger:.3
+})
+
+// Vanilla Tilt js
+VanillaTilt.init(document.querySelectorAll(".card,.image"), {
+    max: 25,
+    speed: 400,
+    glare:true,
+    "max-glare":1,
+});
+
+
